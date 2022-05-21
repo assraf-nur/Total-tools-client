d34648bfc89812b49c1c7e47cc844ea00bc9a6a7
assraf-nur <assraf.nur@gmail.com>
2022-05-22 00:12:33 +0600

    add a banner

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React from "react";
+import banner2 from '../../Images/banner2.jpg'
+import banner1 from '../../Images/banner1.jpg'
+import banner3 from '../../Images/banner3.jpg'
 
 const Banner = () => {
-    return (
-        <div>
-            <h2>Banner</h2>
+  return (
+    <div>
+      <div class="carousel" style={{height: '550px'}}>
+        <div id="item1" class="carousel-item w-full">
+          <img
+            src={banner1}
+            class="w-full" alt=""
+          />
         </div>
-    );
+        <div id="item2" class="carousel-item w-full">
+          <img
+            src={banner2}
+            class="w-full" alt=""
+          />
+        </div>
+        <div id="item3" class="carousel-item w-full">
+          <img
+            src={banner3}
+            class="w-full" alt=""
+          />
+        </div>
+      </div>
+      <div class="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" class="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" class="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" class="btn btn-xs">
+          3
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from './Banner';
 
 
 const Home = () => {
     return (
-        <div className='px-12'>
-            <h2>Home</h2>
+        <div>
+            <Banner></Banner>
         </div>
     );
 };
