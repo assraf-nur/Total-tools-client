43c8ea8d09af138bb176571235c5d2df44b90c36
assraf-nur <assraf.nur@gmail.com>
2022-05-24 12:40:24 +0600

    add review

--- src/Components/Dashboard/MyOrder.js
+++ src/Components/Dashboard/MyOrder.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const MyOrder = () => {
   const [user] = useAuthState(auth);
   const [orders, setOrders] = useState([]);
-  const navigate = useNavigate();
+
 
   useEffect(() => {
     if (user) {
