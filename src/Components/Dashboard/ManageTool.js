1716c5f5b9644aa2a8e32f17a892d2cc04f72cab
assraf-nur <assraf.nur@gmail.com>
2022-05-24 16:11:32 +0600

    add sweet alert

--- src/Components/Dashboard/ManageTool.js
+++ src/Components/Dashboard/ManageTool.js
@@ -1,44 +1,61 @@
-import React from 'react';
-import { toast } from 'react-toastify';
+import React from "react";
+import Swal from "sweetalert2";
 
-const ManageTool = ({tool, refetch}) => {
-    const {_id, name, img, price, moq, aq} = tool;
+const ManageTool = ({ tool, refetch }) => {
+  const { _id, name, img, price, moq, aq } = tool;
 
-    const handleDelete = (id) =>{
-        const proceed = window.confirm("Are you sure ?");
-        if(proceed){
-            const url = `http://localhost:5000/tools/${id}`;
-            fetch(url ,{
-                method: 'DELETE',
-            })
-            .then(res => res.json())
-            .then(data => {
-                refetch();
-                toast.success("Remove user successfully")
-            })
-        }
-    }
+  const handleDelete = (id) => {
+    Swal.fire({
+      imageUrl: `${img}`,
+      imageHeight: 200,
+      imageAlt: 'A tall image',
+      title: `Do you want to delete ${name}`,
+      showCancelButton: true,
+      cancelButtonText: "No",
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Saved!", "", "success");
+        const url = `http://localhost:5000/tools/${id}`;
+        fetch(url, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            refetch();
+          });
+      } else if (result.isCanceled) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
 
-    return (
-        <div className="shadow-xl rounded-2xl border-2 relative">
+  };
+
+  return (
+    <div className="shadow-xl rounded-2xl border-2 relative">
       <figure>
-        <img className="p-12 h-2/12"
-          src={img}
-          alt="Shoes"
-        />
+        <img className="p-12 h-2/12" src={img} alt="Shoes" />
       </figure>
       <div class="card-body mb-12">
         <h2 class="card-title font-bold text-2xl">{name}</h2>
         <p className="font-bold">Available Quantity: {aq} unit</p>
         <p className="font-bold">Minimum Order Quantity: {moq} unit</p>
-        <p className="text-xl">Price Per Unit: <span className="text-primary">{price} $</span></p>
+        <p className="text-xl">
+          Price Per Unit: <span className="text-primary">{price} $</span>
+        </p>
 
         <div class="card-actions">
-          <button onClick={() => handleDelete(_id)} class="btn btn-error mx-auto w-12/12 text-white absolute inset-x-0 bottom-0 h-12 mt-12">Delete</button>
+          <button
+            onClick={() => handleDelete(_id)}
+            class="btn btn-error mx-auto w-12/12 text-white absolute inset-x-0 bottom-0 h-12 mt-12"
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default ManageTool;
